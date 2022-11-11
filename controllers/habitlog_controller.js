205194d1d3b6ed39a815ad1f4db8852f07f9c4f4
Sourav <souravmalviya379@gmail.com>
2022-11-11 17:05:32 +0530

    Almost ready to set week view

--- controllers/habitlog_controller.js
+++ controllers/habitlog_controller.js
@@ -1,14 +1,21 @@
 const HabitLog = require('../models/habit_log');
+const Habit = require('../models/habit');
 
 module.exports.showHistory = async function (req, res) {
-    try {
-       let history = await HabitLog.find({});
-       console.log(history)
-       return res.render('weekView', {
-          history
-       });
-    } catch (err) {
-       console.log('Error in fetching from db: ', err);
-    }
- 
+   try {
+      let habits = await Habit.find({});
+      let habitsCount = habits.length;
+      // console.log(habits.length)
+
+      //limiting the result to get only last 6 days data
+      let history = await HabitLog.find({}).limit(habitsCount * 6).sort('-createdAt');
+      //  console.log(history);
+      return res.render('weekView', {
+         habits,
+         history
+      });
+   } catch (err) {
+      console.log('Error in fetching from db: ', err);
+   }
+
 }
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
 const sassMiddleware = require('node-sass-middleware');
+const Habit = require('./models/habit');
+const HabitLog = require('./models/habit_log');
+
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -19,12 +22,51 @@ app.set('views', './views');
 
 app.use(express.urlencoded());
 
+
+let time = (new Date()).toLocaleTimeString().toString();
+// console.log(time);
+let hours;
+let minutes;
+if (time.includes('pm')) {
+    hours = parseInt(time.split(':')[0]) + 12;
+} else {
+    hours = parseInt(time.split(':')[0]);
+}
+minutes = hours * 60 + parseInt(time.split(':')[1]);
+
+//this function is set to push the daily habits status into habitlogs db
+setTimeout(() => {
+    async function pushHistory() {
+        try {
+            let habits = await Habit.find({});
+            // console.log(habits)
+            for (let habit of habits) {
+                HabitLog.create({
+                    habit: habit._id,
+                    status: habit.status,
+                    completionDate: new Date().toLocaleDateString()
+                })
+            }
+        } catch (err) {
+            console.log('Error in pushing to history : ', err);
+        }
+    }
+    pushHistory();
+
+    //this function will execute in every interval of 24 hours to create the history of daily habits
+    setInterval(pushHistory, 24*60*60*1000);
+        // setInterval(pushHistory, 100000);
+
+    }, (24*60 - minutes - 2 )*60*1000);
+        // }, 3000);
+
+
 app.use('/', require('./routes'));
 
-app.listen(port, function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log('Cannot start server');
-    }else{
-        console.log('Server running at port : '+port);
+    } else {
+        console.log('Server running at port : ' + port);
     }
 });
